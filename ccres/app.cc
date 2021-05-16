451bc1efdd614bad908e88190ba7c4339e828195
narupo <narupo.glab@gmail.com>
2021-05-16 17:06:33 +0900

    add usage

--- ccres/app.cc
+++ ccres/app.cc
@@ -79,9 +79,19 @@ void App::_show_usage() const {
     std::cout << "\n";
     std::cout << "The options are:\n";
     std::cout << "\n";
-    std::cout << "    --help    Show usage.\n";
-    std::cout << "    --json    Dump json format.\n";
-    std::cout << "    --pre     Render until preprocess.\n";
+    std::cout << "           -h, --help    Show usage.\n";
+    std::cout << "               --json    Dump json format.\n";
+    std::cout << "                --pre    Render until preprocess.\n";
+    std::cout << "    --multi-line-name    Parse multi line name.\n";
+    std::cout << "      --render-tokens    Render tokens.\n";
+    std::cout << "               --auto    automatic parse.\n";
+    std::cout << "               --auto    automatic parse.\n";
+    std::cout << "         --off-number    Off number on parse.\n";
+    std::cout << "           --off-name    Off name on parse.\n";
+    std::cout << "           --off-date    Off date on parse.\n";
+    std::cout << "          --off-youbi    Off youbi on parse.\n";
+    std::cout << "           --off-time    Off time on parse.\n";
+    std::cout << "             --off-id    Off id on parse.\n";
     std::cout << std::endl;
 }
 
